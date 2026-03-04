c7a8385736b4189887a7ba7fe290b6b225db3af0
24DP2Mcere <A240480MC@rvt.lv>
2026-03-04 13:37:10 +0200

    Java-mooc:part6-2(final final)

--- src/main/java/rvt/TodoList.java
+++ src/main/java/rvt/TodoList.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 import java.io.*;
 
 public class TodoList {
-    ArrayList<String> saraksts = new ArrayList<>();
+    private ArrayList<String> saraksts = new ArrayList<>();
     private final String filePath = "src/main/java/rvt/todo.csv";
-    private int id;
-
 
     public TodoList() {
         this.saraksts = new ArrayList<>();
+        loadFromFile();
     }
-
 
     public void print() {
         int i = 1;
@@ -23,9 +21,9 @@ public class TodoList {
         }
     }
 
-
-    public void loadFromFile() {
+    private void loadFromFile() {
         try (Scanner reader = new Scanner(new File(filePath))) {
+            reader.nextLine(); // Ignore header row
             while (reader.hasNextLine()) {
                 saraksts.add(reader.nextLine());
             }
@@ -34,10 +32,12 @@ public class TodoList {
         }
     }
 
-
     public int getLastId() {
         int skaititajs = 0;
+        String x = saraksts.get(skaititajs);
+        String[] masivsID = x.split(",");
         try (Scanner reader = new Scanner(new File(filePath))) {
+
             reader.nextLine(); // Ignore header line
             while (reader.hasNextLine()) {
                 skaititajs++;
@@ -46,20 +46,26 @@ public class TodoList {
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        return skaititajs;
-    }
 
+        
+
+        return Integer.valueOf(masivsID[0]);
+    }
 
     public void add(String task) {
-        int ID = getLastId() + 1 ;
-        saraksts.add(ID + "," + task);
-        updateFile();
-    }
+        saraksts.add(task);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true))) {
 
+            bw.write(getLastId() + 1 + "," + task);
+            bw.newLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public void updateFile() {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
-            for (String uzd : saraksts){
+            for (String uzd : saraksts) {
                 bw.write(uzd);
                 bw.newLine();
             }
@@ -68,22 +74,20 @@ public class TodoList {
         }
     }
 
-
     public void remove(int id) {
-        this.id = 0;
         try (Scanner reader = new Scanner(new File(filePath))) {
             reader.nextLine(); // Ignore header line
+            int index = 0;
             while (reader.hasNextLine()) {
-                this.id++;
-                
-                if (this.id == id) {
-                    saraksts.remove(id);
+                String[] parts = reader.nextLine().split(",");
+                if (Integer.valueOf(parts[0]) == id) {
+                    saraksts.remove(index);
                     updateFile();
                 }
-                reader.nextLine();
+                index++;
             }
         } catch (FileNotFoundException e) {
-            
+
         }
     }
 }
--- src/main/java/rvt/UserInterface.java
+++ src/main/java/rvt/UserInterface.java
@@ -6,28 +6,32 @@ public class UserInterface {
 
     private TodoList list;
     private Scanner reader;
+    final String TASK_ReGEX = "^[A-Za-z]+$";
+
 
     public UserInterface(TodoList list, Scanner reader) {
         this.list = list;
         this.reader = reader;
     }
 
+    
     public void start() {
         while (true) {
             System.out.print("Command: ");
             String command = reader.nextLine();
-
-            if (command.equals("stop")) {
-                break;
-            } else if (command.equals("add")) {
-                System.out.print("To add: ");
-                list.add(reader.nextLine());
-            } else if (command.equals("list")) {
-                list.print();
-            } else if (command.equals("remove")) {
-                System.out.print("Which one is removed? ");
-                list.remove(Integer.valueOf(reader.nextLine()));
-            }
+            
+                if (command.equals("stop")) {
+                    break;
+                } else if (command.equals("add")) {
+                    System.out.print("To add: ");
+                    list.add(reader.nextLine());
+                } else if (command.equals("list")) {
+                    list.print();
+                } else if (command.equals("remove")) {
+                    System.out.print("Which one is removed? ");
+                    list.remove(Integer.valueOf(reader.nextLine()));
+                }
+            
         }
     }
 }
